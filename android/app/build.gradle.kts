e050c47de095d776a42997782eb01074d1a34d45
Mateus Cardoso <serjumano17@gmail.com>
2025-12-16 22:35:27 -0300

    feat: :sparkles: add android config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- android/app/src/main/java/com/example/reactnavigationmodule/MainActivity.kt
+++ android/app/src/main/java/com/example/reactnavigationmodule/MainActivity.kt
@@ -13,18 +13,71 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reactnavigationmodule.ui.theme.ReactNavigationModuleTheme
 
+import android.webkit.WebViewClient
+import android.webkit.WebView
+import android.webkit.WebChromeClient
+import android.graphics.Bitmap
+import android.webkit.ConsoleMessage
+import android.webkit.SslErrorHandler
+import android.net.http.SslError
+import android.util.Log
+import android.webkit.WebSettings
+import androidx.activity.addCallback
+import android.content.res.AssetManager
+import android.webkit.WebResourceRequest
+
 class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
+    private fun logAssets(path: String = "") {
+        try {
+            val assets = assets.list(path)
+            assets?.forEach { name ->
+                val fullPath = if (path.isEmpty()) name else "$path/$name"
+                Log.d("ASSETS", fullPath)
+
+
+            }
+        } catch (e: Exception) {
+            Log.e("ASSETS", "Erro ao listar assets", e)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ReactNavigationModuleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+
+        webView = findViewById(R.id.web_view)
+        // puts access from web view
+        with(webView.settings) {
+            allowFileAccess = true
+            allowContentAccess = true
+            allowFileAccessFromFileURLs = true
+            allowUniversalAccessFromFileURLs = true
+        }
+
+        webView.webViewClient = WebViewClientCustom()
+        webView.webChromeClient = WebChromeClientCustomPoster()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.domStorageEnabled = true
+//        webView.settings.setSupportZoom(true)
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+        webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        webView.settings.setSupportMultipleWindows(false)
+        webView.clearCache(true)
+//        webView.addJavascriptInterface(DeviceProvider(this), "AndroidInterface")
+        logAssets()
+        webView.loadUrl("file:///android_asset/index.html")
+
+
+        onBackPressedDispatcher.addCallback {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                finish()
             }
         }
     }
@@ -33,7 +86,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello little $name!",
         modifier = modifier
     )
 }
@@ -43,5 +96,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ReactNavigationModuleTheme {
         Greeting("Android")
+    }
+}
+
+class WebViewClientCustom : WebViewClient(){
+    override fun shouldOverrideUrlLoading(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): Boolean {
+        request?.url?.let { uri ->
+            // qualquer navegação interna volta para o index.html
+            if (uri.scheme == "file") {
+                view?.loadUrl("file:///android_asset/index.html")
+                return true
+            }
+        }
+        return false
+    }
+}
+
+class WebChromeClientCustomPoster : WebChromeClient() {
+    override fun getDefaultVideoPoster(): Bitmap {
+        return Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888)
+    }
+
+    override fun onConsoleMessage(message: ConsoleMessage): Boolean {
+        Log.d("WebViewConsole", "${message.message()} -- From line ${message.lineNumber()} of ${message.sourceId()}")
+        return true
     }
 }
